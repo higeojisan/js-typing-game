1733564f061ec1e87c42f0f269ed2ac985c1c552
m-kouda <makotokoda82@gmail.com>
2017-07-12 22:38:33 +0900

    キーコードの設定

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@
     'surplus', 'cache', 'available', 'expire', 'value',
     'summer', 'operation', 'development', 'medical', 'submit',
   ];
+  // キーコードのオブジェクト
+  var key_codes = {
+    '65': 'a', '66': 'b', '67': 'c', '68': 'd', '69': 'e', '70': 'f',
+    '71': 'g', '72': 'h', '73': 'i', '74': 'j', '75': 'k', '76': 'l',
+    '77': 'm', '78': 'n', '79': 'o', '80': 'p', '81': 'q', '82': 'r',
+    '83': 's', '84': 't', '85': 'u', '86': 'v', '87': 'w', '88': 'x',
+    '89': 'y', '90': 'z',
+  };
 
   // 経過時間の計算
   function calcElapsedTime() {
@@ -46,4 +54,10 @@
     // 単語を表示する
     displayWord();
   }
+
+  // キーイベント
+  document.addEventListener('keydown', function(e) {
+    // 押された文字の取得
+    key_codes[e.keyCode]);
+  })
 })();
