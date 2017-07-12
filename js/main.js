2a8bb0e49f1d6d4b5b193c35b3c92834b65aa01c
m-kouda <makotokoda82@gmail.com>
2017-07-12 23:25:33 +0900

    終了タイマーの実装

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@
   'use strict';
 
   var time_limit = 10000;
+  var start_time;
+  var timer_id;
   var main = document.getElementById('main');
   // 表示用の単語
   var words = [
@@ -11,11 +13,32 @@
   ];
   // キーコードのオブジェクト
   var key_codes = {
-    '65': 'a', '66': 'b', '67': 'c', '68': 'd', '69': 'e', '70': 'f',
-    '71': 'g', '72': 'h', '73': 'i', '74': 'j', '75': 'k', '76': 'l',
-    '77': 'm', '78': 'n', '79': 'o', '80': 'p', '81': 'q', '82': 'r',
-    '83': 's', '84': 't', '85': 'u', '86': 'v', '87': 'w', '88': 'x',
-    '89': 'y', '90': 'z',
+    '65': 'a',
+    '66': 'b',
+    '67': 'c',
+    '68': 'd',
+    '69': 'e',
+    '70': 'f',
+    '71': 'g',
+    '72': 'h',
+    '73': 'i',
+    '74': 'j',
+    '75': 'k',
+    '76': 'l',
+    '77': 'm',
+    '78': 'n',
+    '79': 'o',
+    '80': 'p',
+    '81': 'q',
+    '82': 'r',
+    '83': 's',
+    '84': 't',
+    '85': 'u',
+    '86': 'v',
+    '87': 'w',
+    '88': 'x',
+    '89': 'y',
+    '90': 'z',
   };
 
   // 経過時間の計算
@@ -23,7 +46,9 @@
     var date = new Date();
     var now = date.getTime();
     var elapsed_time = now - start_time;
+    console.log(elapsed_time);
     if (elapsed_time >= time_limit) {
+      clearInterval(timer_id);
       alert('終了！');
     }
   }
@@ -35,8 +60,8 @@
       started = true;
       var start_date_obj = new Date();
       // 開始時間(ミリ秒)の取得
-      var start_time = start_date_obj.getTime();
-      setInterval(calcElapsedTime, 100);
+      start_time = start_date_obj.getTime();
+      timer_id = setInterval(calcElapsedTime, 100);
       startGame();
     } else {
       return;
@@ -58,6 +83,6 @@
   // キーイベント
   document.addEventListener('keydown', function(e) {
     // 押された文字の取得
-    key_codes[e.keyCode]);
+    console.log(key_codes([e.keyCode]));
   })
 })();
